d6af6544161fd6aaa4745913f5299c83ca88a20a
Ricardo Metzger <r7metzger@gmail.com>
2024-11-05 23:08:57 -0500

    feat: inclusión de pantalla de detalle de certificaciones

--- app/src/main/java/pe/gob/fondepes/demo/portal/certificaciones/CertificadosFragment.kt
+++ app/src/main/java/pe/gob/fondepes/demo/portal/certificaciones/CertificadosFragment.kt
@@ -1,59 +1,51 @@
 package pe.gob.fondepes.demo.portal.certificaciones
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
+import pe.gob.fondepes.demo.portal.certificaciones.adapter.CertificateAdapter
+import pe.gob.fondepes.demo.portal.certificaciones.classes.Certificate
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CertificadosFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CertificadosFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_certificados, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CertificadosFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CertificadosFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val listView: ListView = view.findViewById(R.id.lvCertification)
+        val certificates = arrayListOf(
+            Certificate("Liberación de tortugas",
+                "Manipulación y liberación de tortugas marinas, aves y otras especies de captura incidental",
+                "VIGENTE",
+                "07/11/2024",
+                "GONZALES, JUAN ANTONIO",
+                "07/11/2026"),
+            Certificate("Seguridad en faenas de pesca",
+                "Seguridad en faenas de pesca y primeros auxilios",
+                "VIGENTE",
+                "07/11/2024",
+                "METZGER, RICARDO MARTIN",
+                "07/11/2026")
+        )
+        val adapter = CertificateAdapter(requireContext(), certificates)
+        listView.adapter = adapter
+
+        listView.setOnItemClickListener { _, _, position, _ ->
+            val selectedCertificate = certificates[position]
+            val intent = Intent(requireContext(), CertificateDetailActivity::class.java).apply {
+                putExtra("certificate", selectedCertificate)
             }
+            startActivity(intent)
+        }
     }
+
 }
---
+++ app/src/main/java/pe/gob/fondepes/demo/portal/certificaciones/adapter/CertificateAdapter.kt
@@ -0,0 +1,21 @@
+package pe.gob.fondepes.demo.portal.certificaciones.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import pe.gob.fondepes.demo.portal.certificaciones.R
+import pe.gob.fondepes.demo.portal.certificaciones.classes.Certificate
+
+class CertificateAdapter(context: Context, private val certificates: List<Certificate>) : ArrayAdapter<Certificate>(context, 0, certificates){
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val certificate = certificates[position]
+        val view = convertView ?: LayoutInflater.from(context).inflate(R.layout.certificate_item, parent, false)
+        view.findViewById<TextView>(R.id.txtTitleCertDetail).text = certificate.title
+        view.findViewById<TextView>(R.id.txtDescriptionCertDetail).text = certificate.description
+        view.findViewById<TextView>(R.id.txtDateCertDetail).text = certificate.expirationDate
+        return view
+    }
+}
