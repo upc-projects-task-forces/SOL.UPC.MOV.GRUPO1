2115a8ecbeaad14936c8258448754b28a6b3713d
Armando Picón <picondev@gmail.com>
2024-11-07 03:17:33 -0300

    Clean up

--- app/src/main/java/pe/gob/fondepes/demo/portal/certificaciones/NotificationFragment.kt
+++ app/src/main/java/pe/gob/fondepes/demo/portal/certificaciones/NotificationFragment.kt
@@ -11,10 +11,6 @@ import pe.gob.fondepes.demo.portal.certificaciones.data.Notification
 import pe.gob.fondepes.demo.portal.certificaciones.data.NotificationState
 import pe.gob.fondepes.demo.portal.certificaciones.databinding.FragmentNotificationBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
